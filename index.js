ca798c4760ccb0b1b0e687c828192b41f62dd102
Gregory <gregsoninnovo@gmail.com>
2021-10-19 19:25:53 -0500

    create express app

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('My app')
+const express = require('express');
+// create app
+const app = express();
+// the port to run
+const port = 3000;
+
+// define a route, for the dir and cb for the response
+app.get('/', (req, res) => {
+  res.send('My first server in express');
+});
+
+// the ap need to listen
+app.listen(port, () => {
+  console.log(`Listening in the port ${port}`);
+});

5becdb065c7053e614791eed35d3dfa41fcaf51a
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-19 15:54:30 -0500

    change to mysql db

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,10 +4,10 @@ const setUpModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; //URI FROM THE CONNECTION
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; //URI FROM THE CONNECTION
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   logging: true,
 });
 
